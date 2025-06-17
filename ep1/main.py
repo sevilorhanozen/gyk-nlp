3c2a30783cd047fef0cf65ca71bbcb1d760efa3f
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2025-06-17 20:22:53 +0300

    Gün sonu 17.06

--- ep1/main.py
+++ ep1/main.py
@@ -124,6 +124,21 @@ print(vectorizer2.get_feature_names_out())
 print(X2.toarray())
 
 
+# Fonkisyon 
+
+# pipeline => 
+# 1-Tokenization - lowercasing 
+# 2- Stopwords Temizliği
+# 3- Lemmatization
+# 4- TF-IDF Vektörleştirme
+# 5- Feature isimlerini ve arrayi ekrana yazdır.
+
+# generate a corpus of 10 about AI in english
+corpus = [
+    "Artificial Intelligence is the future.",
+    "AI is changing the world.",
+    "AI is a branch of computer science.",
+]
 
 
 

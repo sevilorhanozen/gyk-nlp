87ece3d819fc49b243b6c8f4185016187226f359
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2025-07-03 20:20:40 +0300

    gün sonu

---
+++ sentiment-analysis/main.py
@@ -0,0 +1,3 @@
+# https://github.com/google-research/google-research/tree/master/goemotions/data/full_dataset
+# Buradkai veri setiyle metin işleme yaparak. Gelen yorumdan o yorumdaki genel duygu tutumunu tahmin eden modeli geliştirelim.
+# 1. fark => Sınıflandırma => 27 farklı duygu türü

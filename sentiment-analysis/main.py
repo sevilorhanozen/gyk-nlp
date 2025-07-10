c2101dbdaa086ea36961262e7f05fe8e2d844fa5
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2025-07-10 19:24:41 +0300

    Metin işleme fonksiyonları eklendi; metin temizleme ve etiket artırma işlemleri için yeni yöntemler tanımlandı. Ayrıca, veri seti üzerinde örnek metinler ile işlem sonuçları yazdırıldı.

--- sentiment-analysis/main.py
+++ sentiment-analysis/main.py
@@ -2,6 +2,7 @@
 # Buradkai veri setiyle metin işleme yaparak. Gelen yorumdan o yorumdaki genel duygu tutumunu tahmin eden modeli geliştirelim.
 # 1. fark => Sınıflandırma => 27 farklı duygu türü
 import datasets
+import text_processor
 
 #split => Veriyi böl
 data = datasets.load_dataset("go_emotions", split="train[:5000]")
@@ -9,3 +10,29 @@ data = datasets.load_dataset("go_emotions", split="train[:5000]")
 texts = data['text']
 labels = data['labels']
 
+clean_texts = [text_processor.process_text(text) for text in texts]
+augmented_texts = [text_processor.augment_text(text) for text in clean_texts]
+
+print(texts[0])
+print(clean_texts[0])
+print(augmented_texts[0])
+
+final_texts = clean_texts + augmented_texts
+final_labels = list(labels) + list(labels)
+
+print(len(final_texts))
+print(len(final_labels))
+
+print(final_texts[0])
+print(final_labels[0])
+
+# Eğer labelimiz "sad,happy,love" gibi bir text label ise. [sad,happy,love,angry] => Encoding
+
+# sad => 0
+# happy => 1
+
+# Eğer birden fazlaa label varsa bir veri için. "sad","angry"
+# MultiLabelBinarizer
+# OneHotEncoder
+
+# 19:40
--- sentiment-analysis/text_processor.py
+++ sentiment-analysis/text_processor.py
@@ -1,12 +1,15 @@
 import re
 import nltk
-
+import random 
 nltk.download("stopwords")
 
 stopwords = nltk.corpus.stopwords.words('english')
 
-def process(text):
-    pass
+def process_text(text):
+    text = clean_text(text)
+    text = remove_mention(text)
+    text = remove_stopwords(text)
+    return text.strip()
 
 def clean_text(text):
     text = text.lower()
@@ -14,6 +17,7 @@ def clean_text(text):
     text = re.sub(r'\d+', '', text) # Sayıları kaldır
     text = re.sub(r"[^a-zA-Z0-9\s]", "", text) # Noktalama işaretlerini kaldır
     return text
+
 def remove_mention(text):
     text = re.sub(r"@\S+", "", text) # metindeki mentionleri kaldır
     return text
@@ -21,3 +25,23 @@ def remove_mention(text):
 def remove_stopwords(text):
     text = " ".join([word for word in text.split() if word not in stopwords])
     return text
+
+def augment_text(text):
+    words = text.split()
+    random.shuffle(words)   
+    return " ".join(words) 
+
+# Data Augmentation
+
+#Random Shuffle Data Augmentation
+
+# I Feel very sad => sad label
+# feel I very sad => sad label
+# sad very I feel => sad label
+
+# Synonym Replacement 
+# Random Insertion
+# Random Swap
+# Random Deletion
+# EDA
+# Back Translation

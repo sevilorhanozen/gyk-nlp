36a490fdea42cf5b79da49ffc67b790961c002db
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2025-07-12 10:56:31 +0300

    Modelin performansını artırmak için yeni veri artırma yöntemleri eklendi. Ayrıca, gereksiz yorumlar temizlendi ve kodun okunabilirliği artırıldı.

---
+++ sentiment-analysis/augmentation.py
@@ -0,0 +1,33 @@
+# 1. amaç => Veriyi artırmak
+# 2. amaç => Modelin aynı veriyi farklı varyasyonlarla öğrenmesi.
+
+# Döngü ile tüm veriyi aynı fonk. vermek.
+
+import nlpaug.augmenter.word as naw
+import random
+
+random.seed(35)
+
+text = "The weather is nice today and we should go for a walk."
+
+# Random Synonym Augmenter
+synonm_aug = naw.SynonymAug(aug_src="wordnet")
+text_synonym = synonm_aug.augment(text)
+#
+print(text_synonym)
+
+# Random Swap
+swap_aug = naw.RandomWordAug(action="swap")
+text_swap = swap_aug.augment(text)
+print(text_swap)
+#
+
+# Random Deletion
+deletion_aug = naw.RandomWordAug(action="delete")
+text_deletion = deletion_aug.augment(text)
+print(text_deletion)
+#
+
+
+
+
